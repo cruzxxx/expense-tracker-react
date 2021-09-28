cecca7c3caae599a93ca8783085ef5a3bf866e72
Stef Cruz <crzstefanie@gmail.com>
2021-09-28 20:42:47 +0100

    Learn composition - children props creating a wrapper component and getting rid of duplication of code

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import './Card.css';
+
+function Card(props) {
+    /* This custom component works as a JSX tag such as <div>. */
+
+    //Makes the custom component to accept the card and other classes
+    const classes = 'card ' + props.className;
+
+    return <div className={classes}>{props.children}</div>;
+}
+
+export default Card;
